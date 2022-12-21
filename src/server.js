3a6f762dcb1aa3071646968716d80db7b0fb0ded
sim wook <astralkaiser89@gmail.com>
2022-12-21 20:53:29 +0900

    request, response test

--- src/server.js
+++ src/server.js
@@ -4,15 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
-    return res.send("<h1>I still love you.</h1>");
+    return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
     console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
