1b345c8a71c4bdd46ababd04c6d7bb65ad6d0229
sim wook <astralkaiser89@gmail.com>
2022-12-23 17:16:05 +0900

    controllers create

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
+import {edit, remove} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleEdit = (req, res) => res.send("Edit user");
-const handleDelete = (req, res) => res.send("Delete user");
-
-userRouter.get("/edit", handleEdit);
-userRouter.get("/delete", handleDelete);
+userRouter.get("/edit", edit);
+userRouter.get("/delete", remove);
 
 export default userRouter;
