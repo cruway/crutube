242c360b79c3e86e6624a1ecbd5873a1a7f1cff2
sim wook <astralkaiser89@gmail.com>
2022-12-28 16:37:04 +0900

    mixin update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,33 @@ const fakeUser = {
     loggedIn: true
 }
 
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser });
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        }, {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        }, {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        }
+    ];
+    return res.render("home", {pageTitle: "Home", fakeUser, videos });
+}
 export const see = (req, res) => res.render("watch", {pageTitle: "watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "edit"});
 export const search = (req, res) => res.send("Search");
