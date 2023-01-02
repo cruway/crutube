9a82961cb1d140d0db8918bb4a022306803d8d1e
sim wook <astralkaiser89@gmail.com>
2023-01-03 00:04:52 +0900

    mongo db update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,17 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-    Video.find({}, (error, videos) => {
-        console.log("errors", error);
-        console.log("videos", videos);
-        return res.render("home", {pageTitle: "Home", videos: [] });
-    });
-}
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+};
 export const watch = (req, res) => {
     const {id} = req.params;
     return res.render("watch", {pageTitle: `Watching`});
-}
+};
 export const getEdit = (req, res) => {
     const {id} = req.params;
     return res.render("edit", {pageTitle: `Editing}`});
-}
+};
 export const postEdit = (req, res) => {
     const {id} = req.params;
     const {title} = req.body;
