025c391a4cb72c058fc224d59f7a23c6faa8fa41
sim wook <astralkaiser89@gmail.com>
2023-01-04 22:14:09 +0900

    static add

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,8 +11,8 @@ const videoSchmea = new mongoose.Schema({
     },
 });
 
-videoSchmea.pre('save', async function() {
-    this.hashtags = this.hashtags[0]
+videoSchmea.static('formatHashtags', function(hashtags) {
+    return hashtags
         .split(",")
         .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
