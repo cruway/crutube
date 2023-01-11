22167e6e0526108c0c164b5b8133f1fd73e57928
sim wook <astralkaiser89@gmail.com>
2023-01-11 17:53:11 +0900

    login complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,21 @@
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import {response} from "express";
+import user from "../models/User";
 
 export const getJoin = (req, res) =>
-    res.render("join", { pageTitle: "Join"});
+    res.render("join", {pageTitle: "Join"});
 export const postJoin = async (req, res) => {
-    const { name, username, email, password, password2, location } = req.body;
+    const {name, username, email, password, password2, location} = req.body;
     const pageTitle = "Join";
-    if(password !== password2) {
+    if (password !== password2) {
         return res.status(400).render("join", {
             pageTitle,
             errorMessage: "Password confirmation does not match.",
         });
     }
-    const exists = await User.exists({ $or: [{username}, {email}] });
+    const exists = await User.exists({$or: [{username}, {email}]});
     if (exists) {
         return res.status(400).render("join", {
             pageTitle,
@@ -29,7 +31,7 @@ export const postJoin = async (req, res) => {
             location
         });
         return res.redirect("/login");
-    } catch(error) {
+    } catch (error) {
         return res.status(400).render("join", {
             pageTitle,
             errorMessage: error._message,
@@ -37,12 +39,12 @@ export const postJoin = async (req, res) => {
     }
 };
 export const getLogin = (req, res) =>
-    res.render("login", { pageTitle: "Login" });
+    res.render("login", {pageTitle: "Login"});
 
 export const postLogin = async (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
     const pageTitle = "Login";
-    const user = await User.findOne({ username });
+    const user = await User.findOne({username});
     if (!user) {
         return res.status(400).render("login", {
             pageTitle,
@@ -50,7 +52,7 @@ export const postLogin = async (req, res) => {
         });
     }
     const ok = await bcrypt.compare(password, user.password);
-    if(!ok) {
+    if (!ok) {
         return res.status(400).render("login", {
             pageTitle,
             errorMessage: "Wrong password"
@@ -90,16 +92,48 @@ export const finishGithubLogin = async (req, res) => {
             },
         })
     ).json();
-    if("access_token" in tokenRequest) {
-        const { access_token}  = tokenRequest;
-        const userRequest = await (
-            await fetch("https://api.github.com/user", {
+    if ("access_token" in tokenRequest) {
+        const {access_token} = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (
+            await fetch(`${apiUrl}/user`, {
                 headers: {
                     Authorization: `token ${access_token}`
                 }
             })
         ).json();
-        console.log(userRequest);
+        const emailData = await (
+            await fetch(`${apiUrl}/user/emails`, {
+                headers: {
+                    Authorization: `token ${access_token}`
+                }
+            })
+        ).json();
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if (!emailObj) {
+            return res.redirect("/login");
+        }
+        const existingUser = await User.findOne({ email: emailObj.email} );
+        if(existingUser) {
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect("/");
+        } else {
+            // create an account
+            const user = await User.create({
+                name: userData.name,
+                username: "test",
+                email: emailObj.email,
+                password: "",
+                socialOnly: true,
+                location: userData.location
+            });
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect("/");
+        }
     } else {
         return res.redirect("/login");
     }
