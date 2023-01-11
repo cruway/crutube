fa7a36a71ec5b60afbbd0b4e6d416699e585ca95
sim wook <astralkaiser89@gmail.com>
2023-01-11 10:49:01 +0900

    login update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
@@ -72,7 +73,7 @@ export const startGithubLogin = (req, res) => {
     return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
+export const finishGithubLogin = async (req, res) => {
     const baseUrl = "https://github.com/login/oauth/access_token";
     const config = {
         client_id: process.env.GH_CLIENT,
@@ -81,7 +82,27 @@ export const finishGithubLogin = (req, res) => {
     }
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    return res.redirect(finalUrl);
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+            },
+        })
+    ).json();
+    if("access_token" in tokenRequest) {
+        const { access_token}  = tokenRequest;
+        const userRequest = await (
+            await fetch("https://api.github.com/user", {
+                headers: {
+                    Authorization: `token ${access_token}`
+                }
+            })
+        ).json();
+        console.log(userRequest);
+    } else {
+        return res.redirect("/login");
+    }
 };
 
 export const edit = (req, res) => res.send("Edit User");
