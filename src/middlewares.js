d8e5bd9e075364bb2fed4f50e57aa79e9498d2c6
sim wook <astralkaiser89@gmail.com>
2023-01-11 23:17:28 +0900

    video upload update

--- src/middlewares.js
+++ src/middlewares.js
@@ -8,7 +8,7 @@ export const localsMiddleware = (req, res, next) => {
 };
 
 export const protectorMiddleware = (req, res, next) => {
-    if(req.session.loggedIn) {
+    if (req.session.loggedIn) {
         return next();
     } else {
         return res.redirect("/login");
@@ -16,13 +16,23 @@ export const protectorMiddleware = (req, res, next) => {
 };
 
 export const publicOnlyMiddleware = (req, res, next) => {
-    if(!req.session.loggedIn) {
+    if (!req.session.loggedIn) {
         return next();
     } else {
         return res.redirect("/");
     }
 };
 
-export const uploadFiles = multer({
-   dest: "uploads/"
+export const avatarUpload = multer({
+    dest: "uploads/avatars/",
+    limits: {
+        fileSize: 3000000,
+    }
+});
+
+export const videoUpload = multer({
+    dest: "uploads/videos/",
+    limits: {
+        fileSize: 10000000,
+    }
 });
