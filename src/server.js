3df9f6aa95698fda5e00d94613de9a5096f1df37
sim wook <astralkaiser89@gmail.com>
2023-01-12 23:11:01 +0900

    scss webpack loader test

--- src/server.js
+++ src/server.js
@@ -19,23 +19,12 @@ app.use(
         secret: process.env.COOKIE_SECRET,
         resave: false,
         saveUninitialized: false,
-        /*cookie: {
-            maxAge: 20000,
-        },*/
         store: MongoStore.create({ mongoUrl: process.env.DB_URL })
     })
 );
-
-app.use((req, res, next) => {
-    req.sessionStore.all((error, sessions) => {
-       console.log(sessions);
-       next();
-    });
-});
-
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
-app.use("/assets", express.static("assets"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
