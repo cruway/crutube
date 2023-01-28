b7ed3ef7652aa6fa2fd56e43f53009d10ecf2297
sim wook <astralkaiser89@gmail.com>
2023-01-28 11:22:43 +0900

    flash message update

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {registerView} from "../controllers/videoController";
+import {createComment, registerView} from "../controllers/videoController";
 
 const apiRouter = new express();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
+apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
 
 export default apiRouter;
